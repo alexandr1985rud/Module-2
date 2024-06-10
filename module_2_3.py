ec5f40402cfd10087c477a4fd516cab84bb21049
alexandr1985rud <alexandr.rud85@yandex.ru>
2024-06-10 22:24:23 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+x = 0
+while x < len(my_list):
+    if my_list[x] < 0:
+        break
+    elif my_list[x] == 0:
+        del my_list[x]
+    print(my_list[x])
+    x += 1
