7731b9063082b2fdd141625f38cd88aa1ce5618e
alexandr1985rud <alexandr.rud85@yandex.ru>
2024-06-12 22:12:09 +0300

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -1,13 +1,13 @@
 
-def get_password(n):
+def password(num):
     pairs = []
     for i in range(1, 21):
         for j in range(i + 1, 21):
-            if n % (i + j) == 0:
+            if num % (i + j) == 0:
                 pairs.append(f'{i}{j}')
     result = ''.join(pairs)
     return result
 
 
-for n in range(3, 21):
-    print(f"{n} - {get_password(n)}")
+for num in range(3, 21):
+    print(f"{num} - {password(num)}")
